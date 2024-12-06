619453304e72a0dfdcb15172fa809c42deede058
maplerain42 <kolesnikova.09.04.09@gmail.com>
2024-12-06 20:01:36 +0300

    сервис

--- src/main/kotlin/service/Service.kt
+++ src/main/kotlin/service/Service.kt
@@ -1,12 +1,15 @@
 package org.example.service
 
+import demo.application.client.CrmClient
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.PathVariable
 
 @Service
-class Service {
+class Service (
+    private val crmClient: CrmClient
+){
     fun increase(number: Int): Int{
-        val n_new = number+1
+        val n_new = number + 1
         return n_new
     }
 }
