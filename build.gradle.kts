a974d8eb7f4d588022dd4f95d34f9db56c3a3503
Maria Nepomyaschaya <marianepom555@gmail.com>
2024-12-06 12:06:32 +0300

    gradle и spring

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,12 @@
 plugins {
-    kotlin("jvm") version "2.0.20"
+    kotlin("jvm") version "2.0.10"
+    id("org.springframework.boot") version "3.3.5"
+    kotlin("plugin.spring") version "1.9.23"
+    id("com.bmuschko.docker-spring-boot-application") version "9.4.0"
+
 }
 
-group = "org.example"
+group = "57project"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -10,12 +14,17 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation(platform("org.springframework.boot:spring-boot-dependencies:3.3.6"))
+    implementation(platform("org.springframework.cloud:spring-cloud-dependencies:2023.0.4"))
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
 kotlin {
     jvmToolchain(17)
 }
