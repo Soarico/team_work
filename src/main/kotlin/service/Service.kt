d3d0d00f45ed02e465b46ded2827b372c02168c6
maplerain42 <kolesnikova.09.04.09@gmail.com>
2024-12-06 19:18:27 +0300

    сервис

---
+++ src/main/kotlin/service/Service.kt
@@ -0,0 +1,12 @@
+package org.example.service
+
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.PathVariable
+
+@Service
+class Service {
+    fun increase(number: Int): Int{
+        val n_new = number+1;
+        return n_new;
+    }
+}
